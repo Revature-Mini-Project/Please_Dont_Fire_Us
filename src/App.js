a8f9c75ef7ca0da24abf0f558b1d33bb3ab8cf54
marcski55 <marc.skwarczynski@revature.net>
2021-09-15 12:06:02 -0700

    Fixed overlap issue so we can click

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ function App() {
   return (
     <div className='App'>
       <main id='circle'>
-        <section id='green'></section>
-        <section id='red'></section>
-        <section id='yellow'></section>
-        <section id='blue'></section>
-        <section id='center'></section>
+        <section
+          onClick={() => console.log('clicked green')}
+          id='green'
+        ></section>
+        <section onClick={() => console.log('clicked red')} id='red'></section>
+        <section
+          onClick={() => console.log('clicked yellow')}
+          id='yellow'
+        ></section>
+        <section
+          onClick={() => console.log('clicked blue')}
+          id='blue'
+        ></section>
+        <section
+          onClick={() => console.log('clicked center')}
+          id='center'
+        ></section>
       </main>
     </div>
   );
