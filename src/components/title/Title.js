167a7be03d47aa4e83b666cf2d2c2d505a568400
mat2718 <matthewterry68.mt@gmail.com>
2021-09-15 16:32:23 -0400

    Create Title.js

---
+++ src/components/title/Title.js
@@ -0,0 +1,10 @@
+export function Title() {
+
+    return(
+        <nav class="navbar fixed-top navbar-light bg-light" style="background-color: #e3f2fd;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">The Simon Game</a>
+            </div>
+        </nav>
+    )
+}
