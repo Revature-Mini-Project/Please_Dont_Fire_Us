a8565ea533cf90284f5452e5bca7fe3405e10a66
Brandon Kirsch <brandon.kirsch.4@gmail.com>
2021-09-15 14:41:34 -0700

    Fixes persnickety bug
    Level was one phase behind at all times; is fixed now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RED, GREEN, BLUE, YELLOW, getSequence } from './scripts/sequenceGen';
+// import { Success, Failure } from "./components/alerts/Alerts";
 
-const TIME_LIT = 300, TIME_DIM = 30;
+const TIME_LIT = 300,
+  TIME_DIM = 30;
 
 
 /**
- * 
- * @returns 
+ *
+ * @returns
  */
 function App() {
   const [activeButton, setActiveButton] = useState('');
@@ -17,62 +19,65 @@ function App() {
   const [cursor, setCursor] = useState(0);
   const [playback, setPlayback] = useState(false);
 
-  const increaseCursor = () => setCursor(previousState => previousState + 1);
+  const increaseCursor = () => setCursor((previousState) => previousState + 1);
 
   const handleClick = (code) => {
     if (!playback) {
-      console.log(code);
       lightUp(code);
       setTimeout(() => dimAll(), TIME_LIT);
 
       if (code === currentLevel[cursor]) {
         // CORRECT
         increaseCursor();
-        nextRound();
+
+        if (cursor === currentLevel.length - 1) {
+          nextRound();
+        }
 
       } else {
         // INCORRECT
-
       }
     }
-  }
+  };
 
   // Unless given a true value, gets one additional random value for the next level
   const nextRound = (firstRound = false) => {
+    let sequence = [''];
     if (firstRound) {
-      setCurrentLevel(getSequence(1));
+      sequence = getSequence(1);
     } else {
-      setCurrentLevel(getSequence(currentLevel.length + 1, currentLevel));
+      sequence = getSequence(currentLevel.length + 1, currentLevel);
+      console.log(currentLevel);
     }
     setCursor(0);
-    handleRecite();
+    setCurrentLevel(sequence);
+    setTimeout(() => handleRecite(sequence), 400);
   }
 
   // Sets one button as lit; only one can be lit at a time by this
   const lightUp = (code) => {
     // SOUNDS GO HERE
     setActiveButton(code);
-  }
+  };
 
   // Dims all buttons
   const dimAll = () => {
     setActiveButton('');
-  }
+  };
 
-  
   // Lights all buttons in the current level in order and then sets playback to false
-  const handleRecite = () => {
+  const handleRecite = (sequence = currentLevel) => {
     let recCursor = 0;
     const interval = setInterval(() => {
-      lightUp(currentLevel[recCursor])
+      lightUp(sequence[recCursor])
       recCursor++;
-      if (recCursor >= currentLevel.length) {
+      if (recCursor > sequence.length) {
         clearInterval(interval)
         setPlayback(false);
-      };
+      }
       setTimeout(() => dimAll(), TIME_LIT - TIME_DIM);
     }, TIME_LIT);
-  }
+  };
 
   return (
     <div className='App'>
@@ -86,7 +91,7 @@ function App() {
           onClick={() => handleClick(RED)}
           className={activeButton === 'red' ? 'red-active' : null}
           id='red'
-          ></section>
+        ></section>
         <section
           onClick={() => handleClick(YELLOW)}
           className={activeButton === 'yellow' ? 'yellow-active' : null}
@@ -97,10 +102,11 @@ function App() {
           className={activeButton === 'blue' ? 'blue-active' : null}
           id='blue'
         ></section>
-        <section
-          onClick={null}
-          id='center'
-        ></section>
+        <section id='center'>
+          <button id='start' onClick={() => nextRound(true)}>
+            START
+          </button>
+        </section>
       </main>
     </div>
   );
