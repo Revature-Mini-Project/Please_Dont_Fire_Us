63ea204532ace875d82558065872203fb8108612
mat2718 <matthewterry68.mt@gmail.com>
2021-09-21 13:58:25 -0400

    minor file type changes from js to jsx

--- src/components/alerts/Alerts.jsx
+++ src/components/alerts/Alerts.jsx
@@ -2,35 +2,33 @@ import useSound from "use-sound";
 import React from "react";
 import { Alert } from "reactstrap";
 // sounds
-import success from '../../sounds/success.mp3';
-import failure from '../../sounds/failure.mp3';
-
+import success from "../../sounds/success.mp3";
+import failure from "../../sounds/failure.mp3";
 
 /**
  *  created by Mat Terry
  *  Resources:
  *      reactstrap: https://reactstrap.github.io/components/alerts/
- * @returns 
+ * @returns
  */
 export const Success = () => {
-    
-    return(
-        <div>
-            <Alert color="success">Congratulations! You're a winner!!</Alert>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Alert color="success">Congratulations! You're a winner!!</Alert>
+    </div>
+  );
+};
 
 /**
  *  created by Mat Terry
  *  Resources:
  *      reactstrap: https://reactstrap.github.io/components/alerts/
- * @returns 
+ * @returns
  */
 export const Failure = () => {
-    return(
-        <div>
-            <Alert color="danger">Oh no! You're a failure!</Alert>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Alert color="danger">Oh no! You're a failure!</Alert>
+    </div>
+  );
+};
--- src/components/cards/TheGame.jsx
+++ src/components/cards/TheGame.jsx
@@ -1,16 +1,16 @@
 /**
  * creaated by Marc and Matt
  * utilizes bootstrap to create a card
- * @returns 
+ * @returns
  */
 export function Directions() {
   return (
     <div>
-      <div class='card'>
-        <div class='card-body'>
-          <h5 class='card-title'>The Simon Game</h5>
-          <h6 class='card-subtitle mb-2 text-muted'>Description</h6>
-          <p class='card-text'>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">The Simon Game</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Description</h6>
+          <p class="card-text">
             Get ready to watch, remember, repeat! The Simon game is the exciting
             electronic game of lights and sounds in which players must repeat
             random sequences of lights by pressing the colored pads in the
@@ -19,7 +19,7 @@ export function Directions() {
             advance to higher levels. Keep track of your score as you challenge
             friends or try to beat your own high score.
           </p>
-          <h6 class='card-subtitle mb-2 text-muted'>
+          <h6 class="card-subtitle mb-2 text-muted">
             To begin simply press the start button in the center of the screen.
           </h6>
         </div>
